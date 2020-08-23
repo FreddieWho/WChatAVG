52885741e28f3bb9c24ebf8f6401feb67f274ff1
freddiewho <freddiewho@163.com>
2020-08-24 00:35:23 +0800

    0824

--- character.py
+++ character.py
@@ -1,10 +1,14 @@
-class Player:
-    def __init__(self, name, character, looks, appearance):
-       self.name = name 
-       self.character = character
-       self.looks = looks
-       self.appearance = appearance
-       
-       
+from utils import keyboard
 
+
+class Player:
+    def create(self, name, character, looks, appearance):
+        name = input('你的名字是：')
+        character = input('你的职业是：')
+        looks = input('你的外形是：')
+        appearance = input('你的长相是：')
+        self.name = name
+        self.character = character
+        self.looks = looks
+        self.appearance = appearance
 
