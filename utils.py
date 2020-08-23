90095f2bb4ef6d05c649d9ea1e37d694f0000a0c
freddiewho <freddiewho@163.com>
2020-08-24 00:36:50 +0800

    0824

--- utils.py
+++ utils.py
@@ -1,20 +1,21 @@
 import sys
 
+
 class keyboard:
     @staticmethod
     def next():
-        print_green(u'(---敲击任意键继续---)')
+        print(u'(---敲击任意键继续---)')
         input()
 
     @staticmethod
     def confirm():
-        print_green(u'(---按Y确认, 按其他键跳过---)')
+        print(u'(---按Y确认, 按其他键跳过---)')
         user_input = input().strip()
         if str(user_input).lower() == 'y':
             return True
         else:
             return False
-    
+
     @staticmethod
     def input():
         print(u'(---请输入---)')
