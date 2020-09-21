571f3427c116677e697dc6122c7fb46b91869b04
FreddieWho <43490191+FreddieWho@users.noreply.github.com>
2020-09-22 00:25:32 +0800

    map

--- Map/Map.py
+++ Map/Map.py
@@ -3,8 +3,8 @@ class Block:
         self.Id = id
         self.Connected2 = {}
 
-    def addNeighbor(self, nbr, distence):
-        self.Connected2[nbr] = weight
+    def addNeighbor(self, nbr, distance):
+        self.Connected2[nbr] = distance
 
     def getConnections(self):
         return self.Connected2.keys()
@@ -21,10 +21,54 @@ class GlobalMap:
         self.BlockList = {}
         self.numBlock = 0
 
-    def addBlock(self,id):
+    def addBlock(self,Block):
         self.numBlock = self.numBlock + 1
-        newBlock = Block(id)
+
+        if type(Block).__name__ == 'Block':
+            newBlock = Block
+            id = Block.Id
+        else:
+            newBlock = Block(id)
+            
         self.BlockList[id] = newBlock
         return newBlock
     
-    def getBlock(slef,n)
+    def getBlock(self,n):
+        if n in self.BlockList:
+            return self.BlockList[n]
+        else:
+            return None
+
+    def __contains__(self,n):
+        return n in self.BlockList
+    
+    def addEdge(self,fromId,toId,distance = 0):
+        if fromId not in self.BlockList:
+            nv = self.addBlock(fromId)
+        if toId not in self.BlockList:
+            nv = self.addBlock(toId)
+
+        self.BlockList[fromId].addNeighbor(self.BlockList[toId],distance)
+    
+    def getEdge(self):
+        return self.BlockList.keys()
+
+    def __iter__(self):
+        return iter(self.BlockList.values())
+
+g = GlobalMap()
+for i in range(6):
+    g.addBlock(i)
+
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+for v in g:
+   for w in v.getConnections(): 
+      print("( %s , %s , %s )" % (v.getId(), w.getId(),w.Connected2))
